8ad9674be6292b4fe788c2aed2f89de2c924daa1
veronicajg <59001019+veronicajg@users.noreply.github.com>
2022-10-26 12:42:25 -0300

    Update get.rb

--- features/step_definitions/get.rb
+++ features/step_definitions/get.rb
@@ -3,7 +3,7 @@ Dado("que o usuario verifique informacoes") do
    $uri_base = ("https://portal.vr.com.br/api-web/comum/enumerations/VRPAT") //#uri_base é a variável da url para resgatar o get// 
    
 end
-//# buscando o serviço da API. Onde que vai imprimir no console o "response" que será retornado pelo get//
+//#buscando o serviço da API. Onde que vai imprimir no console o "response" que será retornado pelo get//
    
 
 Quando("ele realizar a pesquisa") do
@@ -13,10 +13,11 @@ end
 //# ação de buscar o serviço da API. Envio  de requisição do Endpoint GET /que o usuário verifique informações e guardando o retorno na variável "response"
 
 Entao('deve procurar a chave typeOfEstablishment') do
-  puts "response body: #{$response.body}" //# validando a resposta da API
-  puts "response message: #{$response.message}" //# validando conteudo das chaves da API
-  puts "response headers: #{$response.headers}" //# validando pacotes de configuração do serviço da API
+  puts "response body: #{$response.body}" //#validando a resposta da API
+  puts "response message: #{$response.message}" //#validando conteudo das chaves da API
+  puts "response headers: #{$response.headers}" //#validando pacotes de configuração do serviço da API
 
 end
 //#essas chaves foram geradas automáticas realizando o comando cucumber no terminal, posteriormente copiar e colar no documento get.br dentro da pasta step_definitions 
 //#(lembre-se de apagar os pendings que vem no meio das chaves criadas automaticamente)
+//#O "Então" são as asserções
